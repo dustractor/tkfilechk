9353bc41e7b3f3c758fad85a7361389370806472
Shams Kitz <dustractor@gmail.com>
2023-08-30 13:54:35 -0500

    Update tkfilechk.cmd

--- tkfilechk.cmd
+++ tkfilechk.cmd
@@ -2,4 +2,4 @@
 set PATH=%HOMEDRIVE%%HOMEPATH%\ANACON~1;%PATH%
 set PYTHONHOME=%HOMEDRIVE%%HOMEPATH%\anaconda3
 call conda activate
-%PYTHONHOME%\pythonw.exe .\tkfilechk.py --path D:\DoRen --ext wav --ext mp3 --no-rescan
+%PYTHONHOME%\pythonw.exe .\tkfilechk.py --ext wav --ext mp3 --no-rescan
